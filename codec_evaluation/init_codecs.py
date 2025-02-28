f5a318142578b39340f0743d742bdc84ee041935
342214650@qq.com <342214650@qq.com>
2025-02-28 05:41:54 +0000

    完善了init_codec.

--- codec_evaluation/init_codecs.py
+++ codec_evaluation/init_codecs.py
@@ -6,23 +6,15 @@ from codec_evaluation.codecs.speechtokenizer import SpeechTokenizer
 from codec_evaluation.codecs.wavlm_kmeans import WavLMKmeans
 from codec_evaluation.codecs.wavtokenizer import WavTokenizer
 
-
 def init_codec(
         modelname: str, 
         sample_rate: int, 
         orig_sample_rate: int,
         mode: str, 
-        model_path: str, 
-        model_ckpt_dir: str, 
-        vocos_ckpt_dir: str, 
-        model_path_dir: str, 
+        model_ckpt_dir: str,  
         device: str, 
         num_codebooks: int ,
-        source: str ,
-        config: str ,
-        checkpoint: str ,
         freeze: bool = False,
-        use_vocos: bool = False,
         need_resample: bool = True,
         ):
     """
@@ -32,17 +24,10 @@ def init_codec(
             sample_rate: The sample rate of the input audio
             orig_sample_rate: The original sample rate of the codec
             mode: "quantized_emb" "unquantized_emb",etc.
-            model_path: The path of the model
             model_ckpt_dir: The path of the model checkpoint
-            vocos_ckpt_dir: The path of the vocos checkpoint
-            model_path_dir: The path of the model path
             device: Select the device to use
             num_codebooks: The number of codebooks
-            source: The source of the model(Repository path of the model on Hugging Face)
-            config: The config of the model(Model configuration filename)
-            checkpoint: The checkpoint of the model(Model checkpoint filename)
             freeze: Whether to calculate the gradient(Default is False)
-            use_vocos: Whether to use vocos(Default is False)
             need_resample: Boolean, whether to resample the audio after decoding(Default is True)
         return:
             model: Initialied codec
@@ -59,7 +44,7 @@ def init_codec(
             mode=mode,
             num_codebooks=num_codebooks,
             need_resample=need_resample,
-            model_path=model_path
+            model_ckpt_dir=model_ckpt_dir
         ).to(device)  
     elif modelname == 'encodec':
         model = Encodec(
@@ -67,9 +52,9 @@ def init_codec(
             orig_sample_rate=orig_sample_rate,
             mode=mode,
             num_codebooks=num_codebooks,
-            use_vocos=use_vocos,
+            use_vocos=False,
+            vocos_ckpt_dir=str | None,  
             model_ckpt_dir=model_ckpt_dir,
-            vocos_ckpt_dir=vocos_ckpt_dir,
             need_resample=need_resample
         ).to(device)
     elif modelname == 'mimi':
@@ -91,7 +76,7 @@ def init_codec(
             semantic_vocab_size=8192,
             ddim_sample_step=50,
             cfg_scale=2.0,
-            model_path_dir=model_path_dir,
+            model_ckpt_dir=model_ckpt_dir,
             need_resample=need_resample
         ).to(device)
     elif modelname =='speechtokenizer':
@@ -120,9 +105,6 @@ def init_codec(
             mode=mode,
             num_codebooks=num_codebooks,
             model_ckpt_dir=model_ckpt_dir,
-            source=source,
-            config=config,
-            checkpoint=checkpoint
         ).to(device)
     else:
         raise ValueError(f"Invalid model name: {modelname}")
