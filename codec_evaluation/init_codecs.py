8304bab8b2c5d273d224a458eb24299e8fc7245a
342214650@qq.com <342214650@qq.com>
2025-02-28 04:14:04 +0000

    添加了mode判断，补全了一些权重写入参数。

--- codec_evaluation/init_codecs.py
+++ codec_evaluation/init_codecs.py
@@ -7,19 +7,27 @@ from codec_evaluation.codecs.wavlm_kmeans import WavLMKmeans
 from codec_evaluation.codecs.wavtokenizer import WavTokenizer
 
 
-def init_codec(modelname, sample_rate, mode, device, freeze = False):
+def init_codec(modelname, sample_rate, mode, model_path, model_ckpt_dir, vocos_ckpt_dir, model_path_dir, device, freeze = False):
     """
         Codec initialization
         input:
             modelname: codecname
             sample_rate: The sample rate of the input audio
             mode: "quantized_emb" "unquantized_emb",etc.
+            model_path: The path of the model
+            model_ckpt_dir: The path of the model checkpoint
+            vocos_ckpt_dir: The path of the vocos checkpoint
+            model_path_dir: The path of the model path
             device: Select the device to use
             freeze: Whether to calculate the gradient
         return:
             model: Initialied codec
 
     """
+    modes = ["encode", "decode", "reconstruct", "unquantized_emb","quantized_emb"]
+    if mode not in modes:
+        raise ValueError(f"Mode must be one of the following: {modes}")
+
     if modelname == 'dac':
         model = DAC(
             sample_rate=sample_rate, 
@@ -27,7 +35,7 @@ def init_codec(modelname, sample_rate, mode, device, freeze = False):
             mode=mode,
             num_codebooks=8,
             need_resample=True,
-            model_path=str | None
+            model_path=model_path
         ).to(device)  
     elif modelname == 'encodec':
         model = Encodec(
@@ -36,8 +44,8 @@ def init_codec(modelname, sample_rate, mode, device, freeze = False):
             mode=mode,
             num_codebooks=8,
             use_vocos=False,
-            model_ckpt_dir=None,
-            vocos_ckpt_dir=None,
+            model_ckpt_dir=model_ckpt_dir,
+            vocos_ckpt_dir=vocos_ckpt_dir,
             need_resample=True
         ).to(device)
     elif modelname == 'mimi':
@@ -45,7 +53,7 @@ def init_codec(modelname, sample_rate, mode, device, freeze = False):
             sample_rate=sample_rate, 
             mode=mode,
             num_codebooks=8,
-            model_ckpt_dir=None,
+            model_ckpt_dir=model_ckpt_dir,
             need_resample=True
         ).to(device)
     elif modelname == 'semanticodec':
@@ -56,7 +64,7 @@ def init_codec(modelname, sample_rate, mode, device, freeze = False):
             semantic_vocab_size=8192,
             ddim_sample_step=50,
             cfg_scale=2.0,
-            model_path_dir=str | None ,
+            model_path_dir=model_path_dir,
             need_resample=True
         ).to(device)
     elif modelname =='speechtokenizer':
@@ -65,7 +73,7 @@ def init_codec(modelname, sample_rate, mode, device, freeze = False):
             mode=mode,
             num_codebooks=8, 
             need_resample=True,
-            model_ckpt_dir=None
+            model_ckpt_dir=model_ckpt_dir
         ).to(device)
     elif modelname =='wavlm_kmeans':
         model = WavLMKmeans(
@@ -79,7 +87,7 @@ def init_codec(modelname, sample_rate, mode, device, freeze = False):
             sample_rate=sample_rate, 
             need_resample=True,
             mode=mode,
-            model_ckpt_dir=None,
+            model_ckpt_dir=model_ckpt_dir,
             source="novateur/WavTokenizer-large-unify-40token",
             config="wavtokenizer_smalldata_frame40_3s_nq1_code4096_dim512_kmeans200_attn.yaml",
             checkpoint="wavtokenizer_large_unify_600_24k.ckpt"
@@ -91,4 +99,4 @@ def init_codec(modelname, sample_rate, mode, device, freeze = False):
         for name, params in model.named_parameters():
             params.requires_grad = False
 
-    return model
+    return model.eval()
