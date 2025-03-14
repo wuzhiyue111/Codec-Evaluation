f74bb9f75a5a5ca1660f3dc7731ff0325c0e2d6b
wuzhiyue111 <347100518@qq.com>
2025-03-14 07:27:35 +0000

    music reconstruction

--- codec_evaluation/probe/dataset/GTZAN_dataset.py
+++ codec_evaluation/probe/dataset/GTZAN_dataset.py
@@ -8,24 +8,35 @@ from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from codec_evaluation.utils.utils import find_audios, cut_or_pad
 from einops import reduce
+from torch.nn.utils.rnn import pad_sequence
 
 class GTZANdataset(Dataset):
     def __init__(
         self,
         split,
-        sample_rate,
-        target_sec,
-        n_segments,
-        is_mono,
-        is_normalize,
         audio_dir,
         meta_dir,
+        sample_rate,
+        target_sec,        
+        is_mono = True,
+        is_normalize = False,
     ):
+        """
+            split: train, valid, test
+            sample_rate: audio sample rate
+            target_sec: target segment length (seconds)
+            is_mono: whether to convert to mono
+            is_normalize: whether to normalize the audio
+            audio_dir: audio root path
+            meta_dir: meta root path
+        """
         self.split = split
         self.sample_rate = sample_rate
         self.target_sec = target_sec
-        self.n_segments = n_segments
-        self.target_length = self.target_sec * self.sample_rate
+        if self.target_sec is not None:
+            self.target_length = self.target_sec * self.sample_rate
+        else:
+            self.target_length = None
         self.is_mono = is_mono
         self.is_normalize = is_normalize
         self.audio_dir = audio_dir
@@ -40,7 +51,7 @@ class GTZANdataset(Dataset):
 
     def __getitem__(self, index):
         return self.getitem(index)
-    
+
     def getitem(self, index):
         """
             return:
@@ -50,16 +61,20 @@ class GTZANdataset(Dataset):
         audio_path = self.metadata.iloc[index].iloc[0]
         audio_file = os.path.join(self.audio_dir,audio_path)
 
-        waveform = self.load_audio(audio_file)
-        label = torch.tensor(self.class2id[audio_path.split('/')[0]]).float()
+        segments, pad_mask= self.load_audio(audio_file)
+        label = self.class2id[audio_path.split('/')[0]]
+        labels = []
+        for mask in pad_mask:
+            if mask == 1:
+                labels.append(label)
+            else:
+                labels.append(-100)
+        labels = torch.tensor(labels)
+        if self.target_length is not None:
+            segments = torch.vstack(segments)
 
-        segments = self.split_audio(waveform, self.n_segments)
-        segments = torch.stack(segments, dim=0)
+        return {"audio": segments, "labels":  labels, "n_segments": len(pad_mask)}
 
-        labels = label.unsqueeze(0).repeat(self.n_segments, 1)
-
-        return segments, labels
-    
     def load_audio(
             self,
             audio_file,
@@ -78,7 +93,7 @@ class GTZANdataset(Dataset):
         except Exception as e:
             print(f"Error loading audio file {audio_file}:{e}")
             return None
-        
+
         # Convert to mono if needed
         if waveform.shape[0] > 1 and self.is_mono:
             waveform = torch.mean(waveform, dim=0, keepdim=True)
@@ -87,103 +102,87 @@ class GTZANdataset(Dataset):
         if self.is_normalize:
             waveform = waveform / waveform.abs().max()
 
-        waveform = cut_or_pad(waveform=waveform, target_length=self.target_length)
-        waveform = waveform.squeeze(0)
+        if self.target_length is not None:
+            waveform, pad_mask = cut_or_pad(waveform=waveform, target_length=self.target_length)
+        else:
+            pad_mask = [1]
 
-        return waveform
-    
+        return waveform, pad_mask
 
-    def split_audio(self, waveform, n_segments):
-        """
-        input:
-            waveform:[T];
-            n_segments: the number of segments per audio
-        return:
-            segments:[n_segments,segment_length]
-        """
-        segment_length = self.target_length // n_segments   # length of per segment
-        segments = []
+    def collate_fn(self, batch):
+        audio_list = [item["audio"] for item in batch if item is not None]
+        label_list = [item["labels"] for item in batch if item is not None]
+        n_segments_list = [item["n_segments"] for item in batch if item is not None]
 
-        for i in range(n_segments):
-            start = i * segment_length
-            end = (i + 1) * segment_length
-            segment = waveform[start:end]
-            segments.append(segment)
+        if self.target_length is not None:
+            audio_tensor = torch.vstack(audio_list)
+        else:
+            audio_list = [audio.squeeze(0) for audio in audio_list]
+            audio_tensor = pad_sequence(audio_list, batch_first=True)
 
-        return segments
+        label_tensor = torch.cat(label_list,dim=0)
+
+        return {
+            "audio": audio_tensor,
+            "labels": label_tensor,
+            "n_segments_list": n_segments_list
+        }
 
 
 class GTZANdataModule(pl.LightningDataModule):
-    def __init__(self, dataset_args, batch_size, codec_name, num_workers):
+    def __init__(self,
+            dataset_args, 
+            codec_name,
+            train_batch_size=32,
+            valid_batch_size=2,
+            test_batch_size=16,
+            train_num_workers=8,
+            valid_num_workers=4,
+            test_num_workers=4):
         super().__init__()
         self.dataset_args = dataset_args
-        self.batch_size = batch_size
+        self.train_batch_size = train_batch_size
+        self.valid_batch_size = valid_batch_size
+        self.test_batch_size = test_batch_size
         self.train_dataset = None
-        self.val_dataset = None
+        self.valid_dataset = None
+        self.test_dataset = None
         self.codec_name = codec_name
-        self.n_segments = dataset_args["n_segments"]
-        self.num_workers = num_workers
-
+        self.train_num_workers = train_num_workers
+        self.valid_num_workers = valid_num_workers
+        self.test_num_workers = test_num_workers
     def setup(self, stage=None):
         if stage == "fit" or stage is None:
             self.train_dataset = GTZANdataset(split="train", **self.dataset_args)
-            # 创建验证集
             self.valid_dataset = GTZANdataset(split="valid", **self.dataset_args) 
-    
-    def train_collate_fn(self, batch):
-        """
-        return:
-            features_tensor:(batch_size * n_segments, length)
-            labels_tensor:(batch_size * n_segments, 10)
-            if codecname='semanticodec'
-                labels_tensor:(batch_size * n_segments * 2, 10)
-        """
-        features, labels = zip(*batch)
-        features_tensor = torch.cat(features, dim=0)
-        labels_tensor = torch.cat(labels, dim=0)
-        labels_tensor = labels_tensor.squeeze(1)  #(B,1)
+        if stage == "val":
+            self.valid_dataset = GTZANdataset(split="valid", **self.dataset_args)
+        if stage == "test":
+            self.test_dataset = GTZANdataset(split="test", **self.dataset_args)
 
-        if self.codec_name == "semanticodec":
-            labels_tensor = torch.cat([labels_tensor, labels_tensor], dim=0)
-
-        return features_tensor, labels_tensor
-    
-    def valid_collate_fn(self, batch):
-        """
-        return:
-            features_tensor:(batch_size * n_segments, length)
-            labels_tensor:(batch_size , 10)
-            if codecname='semanticodec'
-                labels_tensor:(batch_size * 2, 10)
-        """
-        features, labels = zip(*batch)
-        features_tensor = torch.cat(features, dim=0)
-        labels_tensor = torch.cat(labels, dim=0)
-        labels_tensor = labels_tensor.squeeze(1)
-        # labels_tensor = reduce(
-        #     labels_tensor, "(b g) n -> b", reduction="mean", g=self.n_segments
-        # )
-
-
-        if self.codec_name == "semanticodec":
-            labels_tensor = torch.cat([labels_tensor, labels_tensor], dim=0)
-
-        return features_tensor, labels_tensor
-    
     def train_dataloader(self):
         return DataLoader(
             dataset=self.train_dataset,
-            batch_size=self.batch_size,
+            batch_size=self.train_batch_size,
             shuffle=True,
-            collate_fn=self.train_collate_fn,
-            num_workers=self.num_workers,
+            collate_fn=self.train_dataset.collate_fn,
+            num_workers=self.train_num_workers,
         )
 
     def val_dataloader(self):
         return DataLoader(
             dataset=self.valid_dataset,
-            batch_size=2,
+            batch_size=self.valid_batch_size,
             shuffle=False,
-            collate_fn=self.valid_collate_fn,
-            num_workers=self.num_workers,
+            collate_fn=self.valid_dataset.collate_fn,
+            num_workers=self.valid_num_workers,
+        )
+
+    def test_dataloader(self) :
+        return DataLoader(
+            dataset=self.test_dataset,
+            batch_size=self.test_batch_size,
+            shuffle=False,
+            collate_fn=self.test_dataset.collate_fn,
+            num_workers=self.test_num_workers,
         )
---
+++ codec_evaluation/reconstruction_eval/reconstruction_music_eval.py
@@ -0,0 +1,225 @@
+import os
+import random
+import numpy as np
+import torch
+import torchaudio
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data.dataloader import DataLoader
+from codec_evaluation.probe.dataset.GTZAN_dataset import (
+    GTZANdataset,
+)
+from tqdm import tqdm
+from codec_evaluation.init_codecs import init_codec
+from typing import Optional
+from codec_evaluation.reconstruction_eval.utils import (
+    calculate_pesq,
+    calculate_stoi,
+)
+from tqdm import tqdm
+
+
+def seed_all(seed):
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)  # for multi-GPU setups
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.backends.cudnn.deterministic = False
+    torch.backends.cudnn.benchmark = False
+
+
+seed_all(666)
+
+
+class CodecEvaluation:
+    def __init__(
+        self,
+        codec_name: str,
+        model_ckpt_dir: str,
+        device: str,
+        sample_rate: int,
+        dataset_audio_dir: str,
+        dataset_meta_dir: str,
+        batch_size: int = 32,
+        num_workers: int = 8,
+        mode: str = "reconstruct",
+        **kwargs,
+    ):
+        """
+        codec_model_safetensors_path: codec absolute path model.safetensors
+        asr_model_path_or_name: asr model path or name for wer compute
+        wav2vec_model_path_or_name: wav2vec model for computing spk_sim
+        dataset_meta_path: absolute path to dataset_meta json
+        dataset_audio_path: where the audio root path is
+        sample_rate: audio sample rate
+        device: cuda:0 or cpu
+        batch_size: batch size
+        codec_trunk_size: the trunk size (seconds) of extract code
+        """
+        self.codec = init_codec(
+            modelname=codec_name,
+            sample_rate=sample_rate,
+            mode=mode,
+            model_ckpt_dir=model_ckpt_dir,
+            device=device,
+            freeze=True,
+            need_resample=False,  # return the audio resampled to codec sample rate
+            **kwargs,
+        )
+
+        self.codec_sample_rate = self.codec.orig_sample_rate
+        self.sample_rate = sample_rate
+
+        self.batch_size = batch_size
+        self.device = device
+        dataset = GTZANdataset(
+            audio_dir=dataset_audio_dir,
+            split="test",
+            meta_dir=dataset_meta_dir,
+            sample_rate=sample_rate,
+            target_sec=None,
+        )
+        self.dataloader = DataLoader(
+            dataset,
+            batch_size=batch_size,
+            shuffle=False,
+            num_workers=num_workers,
+            collate_fn=dataset.collate_fn,
+        )
+
+    @torch.inference_mode()
+    def evaluate(self):
+        # first get the reconstruction audio
+        gt_audio_list = []
+        rec_audio_list = []
+        print(f"dataset length: {len(self.dataloader)}, now start to reconstruct")
+        for batch in tqdm(self.dataloader, desc="reconstruct audio"):
+            gt_audio_test = batch["audio"].clone()
+            # resample to codec sample rate
+            if self.codec_sample_rate != self.sample_rate:
+                resampler = torchaudio.transforms.Resample(
+                    self.sample_rate, self.codec_sample_rate
+                ).to(gt_audio_test.device)
+                gt_audio_test = resampler(gt_audio_test)
+
+            gt_audio = batch["audio"].to(self.device)
+            max_length = gt_audio.shape[-1]
+
+            if self.codec_sample_rate != self.sample_rate:
+                max_length = int(
+                    max_length * (self.codec_sample_rate / self.sample_rate)
+                )
+
+            # prevent the more than max_length
+            rec_audio = self.codec(gt_audio)[:, :max_length]
+
+            # prevent the less than gt_audio
+            min_length = min(gt_audio_test.shape[-1], rec_audio.shape[-1])
+
+            # save to list
+            for gt_audio_t, rec_a in zip(gt_audio_test, rec_audio):
+                gt_audio_list.append(gt_audio_t[:min_length].to("cpu"))
+                rec_audio_list.append(rec_a[:min_length].to("cpu"))
+
+        torch.cuda.empty_cache()
+        print(f"reconstruct done, now start to compute metrics")
+
+        # compute metrics
+        stoi_list = []
+        pesq_list = []
+
+        data_length = len(gt_audio_list)
+        for i in tqdm(
+            range(0, data_length, 50), desc="compute metrics"
+        ):  # per 50 samples to compute metrics
+            if i + 50 < data_length:
+                tmp_gt_audio_list = gt_audio_list[i : i + 50]
+                tmp_rec_audio_list = rec_audio_list[i : i + 50]
+            else:
+                tmp_gt_audio_list = gt_audio_list[i:]
+                tmp_rec_audio_list = rec_audio_list[i:]
+
+            tmp_gt_audio = pad_sequence(tmp_gt_audio_list, batch_first=True).to(
+                self.device
+            )
+            tmp_rec_audio = pad_sequence(tmp_rec_audio_list, batch_first=True).to(
+                self.device
+            )
+
+            # stoi
+            stoi_list.append(
+                calculate_stoi(
+                    gt_audio=tmp_gt_audio,
+                    rec_audio=tmp_rec_audio,
+                    sample_rate=self.codec_sample_rate,
+                )
+            )
+            print(f"stoi: {stoi_list[-1]}")
+
+            # pesq
+            pesq_list.append(
+                calculate_pesq(
+                    gt_audio=tmp_gt_audio,
+                    rec_audio=tmp_rec_audio,
+                    sample_rate=self.codec_sample_rate,
+                )
+            )
+            print(f"pesq: {pesq_list[-1]}")
+
+        avg_stoi = sum(stoi_list) / len(stoi_list)
+        avg_pesq = sum(pesq_list) / len(pesq_list)
+        print(f"compute metrics done, now start to save results")
+
+        print(f"stoi: {avg_stoi}")
+        print(f"pesq: {avg_pesq}")
+        return {
+            "stoi": avg_stoi,
+            "pesq": avg_pesq,
+        }
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--codec_name", type=str, default="dac")
+    parser.add_argument(
+        "--model_ckpt_dir",
+        type=str,
+        default="/sdb/model_weight/codec_evaluation/codec_ckpt/",
+    )
+    parser.add_argument("--device", type=str, default="cuda:0")
+    parser.add_argument("--sample_rate", type=int, default=22050)
+    parser.add_argument(
+        "--dataset_audio_dir",
+        type=str,
+        default="/sdb/data1/music/mix_music/marble_dataset/data/GTZAN/genres",
+    )
+    parser.add_argument(
+        "--dataset_meta_dir",
+        type=str,
+        default="/sdb/data1/music/mix_music/marble_dataset/data/GTZAN",
+    )
+    parser.add_argument("--batch_size", type=int, default=24)
+    parser.add_argument("--num_workers", type=int, default=8)
+    parser.add_argument("--mode", type=str, default="reconstruct")
+    parser.add_argument("--use_vocos", type=bool, default=False)
+    parser.add_argument("--vocos_ckpt_dir", type=Optional[str], default=None)
+    args = parser.parse_args()
+    print(f"args: {args}")
+    codec_eval = CodecEvaluation(
+        codec_name=args.codec_name,
+        model_ckpt_dir=args.model_ckpt_dir,
+        device=args.device,
+        sample_rate=args.sample_rate,
+        dataset_audio_dir=args.dataset_audio_dir,
+        dataset_meta_dir=args.dataset_meta_dir,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+        mode=args.mode,
+        use_vocos=args.use_vocos,
+        vocos_ckpt_dir=args.vocos_ckpt_dir,
+    )
+    result = codec_eval.evaluate()
+    print(f"result: {result}")
